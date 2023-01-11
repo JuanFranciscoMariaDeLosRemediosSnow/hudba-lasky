f3a78955c084b4479baa305a60ec8077996aff5b
JuanFranciscoMariaDeLosRemediosSnow <victor.liptak@gmail.com>
2023-01-11 07:19:49 +0100

    pridal som css

---
+++ js/script.js
@@ -0,0 +1,23 @@
+(function($, window, document, undefined) {
+
+    /*$.fn.parallax = function(options) {
+        var settings = $extend({},options);
+        
+        var $win = $(window);
+
+        return this.each(function() {
+            var element = $(this);
+
+            $win.on('scroll', function() {
+                var bgTop = element.offset().top,
+                    winTop = $win.scrollTop(),
+                    bgPositonLeft = element.css('backgroundPositionX');
+
+                console.log(bgTop, winTop, Math.floor((bgTop - winTop) * 0.2));
+            });
+        });
+    }  
+    
+    $('#one').parallax();*/
+
+})(jQuery);
