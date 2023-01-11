6b6b4677a88fa6cd86d7aaa9a687ceb952f46553
JuanFranciscoMariaDeLosRemediosSnow <victor.liptak@gmail.com>
2023-01-11 15:23:31 +0100

    dokoncene vsetko?

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+$(function(){
+	var menu = $('.vrch'),
+		menuLinks= menu.find('a');
+
+	menuLinks.on('click', function(event) {
+		//toto zabrani aby sme preblikli(isli naprv dole potom hore a az potom animacia scrollovania)
+		event.preventDefault();
+
+        var id = this.hash;
+
+		//animacia scroll na prislusne miesto
+		$('html,body').animate({scrollTop: $(id).offset().top}, 2000, function() {
+			//prida hash do adresy
+			window.location.hash = id;
+		});
+
+	});
+});
+
+
+
+
+
+
 (function($, window, document, undefined) {
 
     /*$.fn.parallax = function(options) {
