01db06799f3e203665cecf35bad5856858ad3854
Juan Snow <victor.liptak@gmail.com>
2023-01-16 15:53:40 +0100

    vytvorenie parallax efektu pre prvu snimku

--- js/script.js
+++ js/script.js
@@ -18,30 +18,8 @@ $(function(){
 });
 
 
-
-
-
-
-(function($, window, document, undefined) {
-
-    /*$.fn.parallax = function(options) {
-        var settings = $extend({},options);
-        
-        var $win = $(window);
-
-        return this.each(function() {
-            var element = $(this);
-
-            $win.on('scroll', function() {
-                var bgTop = element.offset().top,
-                    winTop = $win.scrollTop(),
-                    bgPositonLeft = element.css('backgroundPositionX');
-
-                console.log(bgTop, winTop, Math.floor((bgTop - winTop) * 0.2));
-            });
-        });
-    }  
+(function($) {
     
-    $('#one').parallax();*/
+    $('#top').parallax({ friction: -0.8 });
 
 })(jQuery);
