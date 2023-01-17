74726999b1f671ff1dff0276d6784d527e9deca8
Juan Snow <victor.liptak@gmail.com>
2023-01-17 11:32:09 +0100

    odstranenie nepot.js,uprava scrollovania,upravaprl

--- js/script.js
+++ js/script.js
@@ -1,51 +1,12 @@
-// Animacia scrollovania
-$(function(){
-	var menu = $('.vrch'),
-		menuLinks= menu.find('a');
-
-    $('.h-l').find('a').on('click', function(event) {
-		event.preventDefault();
-    });
-
-    $('.panel').find('a').on('click', function(event) {
-		event.preventDefault();
-    });
-
-    $('.dropdown').find('a').on('click', function(event) {
-		event.preventDefault();
-    });
-
-
-	menuLinks.on('click', function(event) {
-        var id = this.hash;
-
-		//animacia scroll na prislusne miesto
-		$('html,body').animate({scrollTop: $(id).offset().top}, 1000, function() {
-			//prida hash do adresy
-			window.location.hash = id;
-		});
-
-	});
-});
-
-// Zavolanie parallax efektu
-
-(function($) {
-    
-    $('#top').parallax({ friction: 0.3 });
-
-})(jQuery);
 
 // Dropbox
 
 const dropdownBtn = document.getElementById("btn");
 const dropdownMenu = document.getElementById("dropdown");
-//const toggleArrow = document.getElementById("arrow");
 
 // Toggle dropdown function
 const toggleDropdown = function () {
   dropdownMenu.classList.toggle("show");
-  //toggleArrow.classList.toggle("arrow");
 };
 
 // Toggle dropdown open/close when dropdown button is clicked
